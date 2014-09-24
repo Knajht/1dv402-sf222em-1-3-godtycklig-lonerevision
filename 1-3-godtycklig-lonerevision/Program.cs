93e673450390a9b2967e80e1e3b4310c0719e565
Sebastian Fernstedt <sf222em@student.lnu.se>
2014-09-24 21:47:51 +0200

    Built basic ReadInt()

--- 1-3-godtycklig-lonerevision/Program.cs
+++ 1-3-godtycklig-lonerevision/Program.cs
@@ -10,12 +10,19 @@ namespace _1_3_godtycklig_lonerevision
     {
         static void Main(string[] args)
         {
+            int noOfSalaries = ReadInt("Ange antal löner att mata in: ");
+
+
             //Deklarera variabler
             //Input: Antal löner
             //Input: Varje lön
             //Skicka till array
-
-            //
+            //Kopiera till medianarray, sorteras
+            //Beräkna medianlön, presentera
+            //Beräkna medellön, presentera
+            //Beräkna lönespridning, presentera
+            //Presentera de inmatade lönerna
+            //Börja om med tangent - Avsluta med esc
         }
 
         static void ProcessSalaries()
@@ -23,9 +30,26 @@ namespace _1_3_godtycklig_lonerevision
             
         }
 
-        static int ReadInt()
+        static int ReadInt(string prompt)
         {
-            return 0;
+            int count = 0;
+
+            Console.Write(prompt);
+            try
+            {
+                count = int.Parse(Console.ReadLine());
+                if(count < 0)
+                {
+                    throw new SystemException();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Var vänlig ange ett heltal formaterat som siffror.");
+                ReadInt(prompt);
+            }
+            return count;
+            
         }
     }
 }
